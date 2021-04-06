2756470a963aa1dbd682d222c5765e713b6fb28f
Alexander Pico <alex.pico@gladstone.ucsf.edu>
2021-04-05 17:17:51 -0700

    Update scnetviz.R

--- R/scnetviz.R
+++ R/scnetviz.R
@@ -1,1 +1,3 @@
 # TODO: write wrapper for seurat to scnetviz
+# See wrapper steps: https://github.com/satijalab/seurat-wrappers/wiki/Submission-Process
+# See code guidelines: https://github.com/satijalab/seurat-wrappers/wiki/Code-Guidelines
